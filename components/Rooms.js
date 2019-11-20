a4120ff811bc4337e6dde0b72ac8e0414d907b08
Admin <admin@Tests-MacBook-Pro.local>
2019-11-20 18:40:30 +0700

    Load rooms from API, create spinner component and helpers

--- components/Rooms.js
+++ components/Rooms.js
@@ -1,27 +1,24 @@
+import { useState, useEffect } from 'react';
 import Link from './Link';
 import PropTypes from 'prop-types';
+import Spinner from './Spinner';
+
+/* Helpers */
+import { isContainsProtocol } from '../helpers';
 
 const Rooms = ({ showFooter }) => {
-    const rooms = [
-        {
-            id: 1,
-            title: 'Studio Room',
-            description: 'Simply Comfort',
-            img: 'baleroom-background-sm.jpg',
-        },
-        {
-            id: 2,
-            title: '2 Bed Room',
-            description: 'Family Space',
-            img: 'baleroom-background-sm.jpg',
-        },
-        {
-            id: 3,
-            title: 'Executive Room',
-            description: 'Luxuriously Wide',
-            img: 'baleroom-background-sm.jpg',
-        },
-    ];
+    const [rooms, setRooms] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const adminUrl = isContainsProtocol(process.env.ADMIN_URL) ? process.env.ADMIN_URL : 'https://' + process.env.ADMIN_URL;
+
+    useEffect(() => {
+        fetch(adminUrl + '/rooms')
+            .then((res) => res.json())
+            .then((data) => {
+                setRooms(data);
+                setIsLoading(false);
+            });
+    }, [rooms, isLoading]);
 
     return (
         <section id="rooms" className="text-center py-5">
@@ -32,56 +29,63 @@ const Rooms = ({ showFooter }) => {
                 <hr className="divider border-dark mb-3" />
             </div>
 
-            <div className="rooms-body text-white">
-                <div className="container-fluid overflow-hidden">
-                    <div className="row m-min-2">
-                        {rooms.map((room, key) => {
-                            // const col = key < 2 ? 6 : 4;
-                            // const fit = key < 2 ? 'width' : 'height';
+            {isLoading ? (<Spinner />) : (
+                <div>
+                    <div className="rooms-body text-white">
+                        <div className="container-fluid overflow-hidden">
+                            <div className="row m-min-2">
+                                {rooms.map((room) => {
+                                    // const col = key < 2 ? 6 : 4;
+                                    // const fit = key < 2 ? 'width' : 'height';
 
-                            const col = 4;
-                            const fit = 'height';
+                                    const col = 4;
+                                    const fit = 'height';
 
-                            return (
-                                <div className={`col-lg-${col} p-2`} key={room.id}>
-                                    <div className="rooms-item bg-mask-dark">
-                                        <div className="item-img position-relative d-flex justify-content-center align-items-center h-100 overflow-hidden">
-                                            <img src={`/static/images/${room.img}`} alt={room.title} className={`position-absolute z-min-1 fit-${fit}`} />
-                                        </div>
-                                        <div className="item-content border-effect position-absolute top-50 left-50 xy-min-50 d-flex flex-column justify-content-center align-items-center">
-                                            <span>
-                                                <h2 className="h3 mb-0">
-                                                    {room.title}
-                                                </h2>
-                                                <p className="h6 mb-0">
-                                                    {room.description}
-                                                </p>
-                                                
-                                                <div className="item-subcontent">
-                                                    
-                                                    <button className="btn btn-primary mt-3 shadow-sm">
-                                                        MORE INFO <i className="fa fa-chevron-right ml-1"/>
-                                                    </button>
+                                    return (
+                                        <div className={`col-lg-${col} p-2`} key={room.id}>
+                                            <div className="rooms-item bg-mask-dark">
+                                                <div className="item-img position-relative d-flex justify-content-center align-items-center h-100 overflow-hidden">
+                                                    <img src={adminUrl + room.image.url} alt={room.title} className={`position-absolute z-min-1 fit-${fit}`} />
                                                 </div>
-                                            </span>
-                                        </div>
-                                    </div>
-                                </div>
-                            );
-                        })}
-                    </div>
-                </div>
-            </div>
+                                                <div className="item-content border-effect position-absolute top-50 left-50 xy-min-50 d-flex flex-column justify-content-center align-items-center">
+                                                    <span>
+                                                        <h2 className="h3 mb-0">
+                                                            {room.title}
+                                                        </h2>
+                                                        <p className="h6 mb-0">
+                                                            {room.description}
+                                                        </p>
 
-            {showFooter ? (
-                <div className="rooms-footer mt-5">
-                    <Link href="/rooms">
-                        <a role="button" className="btn btn-outline-dark">
-                            VIEW ALL ROOMS
-                        </a>
-                    </Link>
+                                                        <div className="item-subcontent">
+
+                                                            <button className="btn btn-primary mt-3 shadow-sm">
+                                                                MORE INFO <i className="fa fa-chevron-right ml-1" />
+                                                            </button>
+                                                        </div>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                    </div>
+
+                    {showFooter ? (
+                        <div className="rooms-footer mt-5">
+                            <Link href="/rooms">
+                                <a role="button" className="btn btn-outline-dark">
+                                    VIEW ALL ROOMS
+                </a>
+                            </Link>
+                        </div>
+                    ) : null}
                 </div>
-            ) : null}
+            )}
+
+
+
         </section>
     );
 };
